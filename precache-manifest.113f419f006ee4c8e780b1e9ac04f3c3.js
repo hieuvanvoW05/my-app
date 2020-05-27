89b5b15c3eade237642d69f8bb62b359ff7ff69b
HIEU NGUYEH <43071885+hieuvanvoW04@users.noreply.github.com>
2020-05-27 03:07:18 -0300

    Updates

---
+++ precache-manifest.113f419f006ee4c8e780b1e9ac04f3c3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0b1f88e3b73d41523b629b0ae231bc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "999eb4e178ccec822575",
+    "url": "/my-app/static/css/2.5206c547.chunk.css"
+  },
+  {
+    "revision": "e32c62a6736eb2fe8036",
+    "url": "/my-app/static/css/main.b267d973.chunk.css"
+  },
+  {
+    "revision": "999eb4e178ccec822575",
+    "url": "/my-app/static/js/2.fba0c039.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-app/static/js/2.fba0c039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32c62a6736eb2fe8036",
+    "url": "/my-app/static/js/main.90f79b67.chunk.js"
+  },
+  {
+    "revision": "d21a59192a6355194f80",
+    "url": "/my-app/static/js/runtime-main.f5212b54.js"
+  },
+  {
+    "revision": "1b0bed519f452191cc996e9a35352446",
+    "url": "/my-app/static/media/android1.1b0bed51.PNG"
+  },
+  {
+    "revision": "ff9ac464317c98b6e70bd446311d1796",
+    "url": "/my-app/static/media/android2.ff9ac464.PNG"
+  },
+  {
+    "revision": "a80fc895dd3ef08d36b8abcce6b75202",
+    "url": "/my-app/static/media/arrietty.a80fc895.jpg"
+  },
+  {
+    "revision": "875a7bd5fef2153cc26adcf37ed177c8",
+    "url": "/my-app/static/media/background1.875a7bd5.jpg"
+  },
+  {
+    "revision": "f4d079a501ada5fdc1167c20f7aea55e",
+    "url": "/my-app/static/media/background2.f4d079a5.jpg"
+  },
+  {
+    "revision": "2b04daa3dcd883d857ba74b561a4d166",
+    "url": "/my-app/static/media/background3.2b04daa3.jpg"
+  },
+  {
+    "revision": "d0774f63070e620328ecf4d1cab90705",
+    "url": "/my-app/static/media/db3.d0774f63.PNG"
+  },
+  {
+    "revision": "f0e9011c5081ab0b56096d812fc1bdb8",
+    "url": "/my-app/static/media/db4.f0e9011c.PNG"
+  },
+  {
+    "revision": "003a1dac5a3940c2c58d62ae466dabe3",
+    "url": "/my-app/static/media/db5.003a1dac.PNG"
+  },
+  {
+    "revision": "7744cc4cee3e51694993dbcd666e4b2f",
+    "url": "/my-app/static/media/db6.7744cc4c.PNG"
+  },
+  {
+    "revision": "b1d199aa4bdc834ac41bce4dd559f55d",
+    "url": "/my-app/static/media/db7.b1d199aa.PNG"
+  },
+  {
+    "revision": "5cd9f30e245cbdcc11274951e439ad65",
+    "url": "/my-app/static/media/github-logo.5cd9f30e.png"
+  },
+  {
+    "revision": "197de2c00cf6aa4763296589d0727d0f",
+    "url": "/my-app/static/media/javascript1.197de2c0.PNG"
+  },
+  {
+    "revision": "81682b5ed6bfba7219ebb9ea4f0cc05f",
+    "url": "/my-app/static/media/javascript2.81682b5e.PNG"
+  },
+  {
+    "revision": "0336eaf55c9e94ae7c8ea4f333a71e70",
+    "url": "/my-app/static/media/javascript3.0336eaf5.PNG"
+  },
+  {
+    "revision": "5b326b094f001d3dc2b4e8c6b8d5fa68",
+    "url": "/my-app/static/media/nausicaa.5b326b09.jpg"
+  },
+  {
+    "revision": "7d5f08cf2912ccb3c7b10ca5215ea9d3",
+    "url": "/my-app/static/media/whisper-of-the-heart.7d5f08cf.png"
+  }
+]);
